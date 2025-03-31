b361003189602d8877f6aa5d7ff93c757d5e9e65
brow1063 <brow1063@algonquinlive.com>
2025-03-31 14:43:51 -0400

    Added Vnet and Subnet to terraform creation file

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -8,3 +8,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_prefix}-a12-rg"
   location = "westus3"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_prefix}-a12-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.resource_prefix}-a12-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
